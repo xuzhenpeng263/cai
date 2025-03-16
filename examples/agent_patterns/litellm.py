5c551efb23f2d902d2e04a0139a40ec4c8988d62
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2025-03-16 21:01:26 +0100

    Add litellm iterations

--- examples/agent_patterns/litellm.py
+++ examples/agent_patterns/litellm.py
@@ -4,18 +4,24 @@ from agents.model_settings import ModelSettings
 from agents import set_default_openai_client, set_tracing_disabled
 
 external_client = AsyncOpenAI(
-    base_url = 'http://localhost:8000/v1',
-    api_key='ollama', # required, but unused
-)
+    base_url = 'http://localhost:4000',
+    api_key="cai")
 
 set_default_openai_client(external_client)
 set_tracing_disabled(True)
 
+# llm_model="qwen2.5:14b"
+# llm_model="claude-3-7"
+llm_model="gpt-4o"
+
+# For Qwen models, we need to skip system instructions as they're not supported
+instructions = None if "qwen" in llm_model.lower() else "You are a helpful assistant"
+
 agent = Agent(
     name="Assistant", 
-    instructions="You are a helpful assistant",
+    instructions=instructions,
     model=OpenAIChatCompletionsModel(
-        model="qwen2.5:14b",
+        model=llm_model,
         openai_client=external_client,
     )
 )
