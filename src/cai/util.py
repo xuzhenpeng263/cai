384e8449b53b76f4d3ff636f06e8bae2c8599e7b
luijait <luijait@icloud.com>
2025-05-23 10:03:49 +0200

    typo

--- src/cai/util.py
+++ src/cai/util.py
@@ -288,7 +288,7 @@ class CostTracker:
         # Check if this is a local/free model
         model_str = model_name.lower()
         is_local_model = (
-           "alias" not in model_str and
+            "alias" not in model_str and
             "ollama" in model_str or
             "qwen" in model_str or
             "llama" in model_str or
